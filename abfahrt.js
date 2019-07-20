cd9dcc659bb3ada76dc5a403ee15038295efe9bf
Aleksey Lashin <aleksey.lashin@gmail.com>
2019-07-20 02:46:20 +0200

    Prepared for Alexa Skill

---
+++ abfahrt.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const normalize = text => {
+    try {
+        if (text === 'Sofort') {
+            return 'sofort';
+        }
+
+        const minutes = parseInt(text.replace(' Min', ''));
+        if (minutes === 1) {
+            return 'in eine Minute';
+        }
+        return `in ${minutes} Minuten`;
+    } catch (Error) {
+        return text;
+    }
+};
+
+const execute = async () => {
+    try {
+        const response = await axios.get('https://www.kvb.koeln/qr/343/');
+        const $ = cheerio.load(response.data);
+
+        const data = [];
+        $('#qr_ergebnis td.qr_td').each((i, elem) => {
+            data.push($(elem).text());
+        });
+
+        const resultData = [];
+
+        for (let i = 0; i < data.length; i = i + 3) {
+            if (data[i] === '16') {
+                resultData.push(normalize(data[i + 2]));
+            }
+        }
+
+        let result = resultData.join(', ');
+
+        const lastComma = result.lastIndexOf(',');
+
+        if (lastComma !== -1) {
+            result = result.substring(0, lastComma) + ' und' + result.substring(lastComma + 1);
+        }
+
+        result = `Bahn fährt ` + result;
+        return result;
+    } catch (error) {
+        return 'Etwas ist schief gelaufen';
+    }
+};
+
+module.exports = execute;
--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const abfahrt = require('./abfahrt');
 
-const execute = async () => {
-    const response = await axios.get(`https://www.kvb.koeln/qr/343/`);
-    $ = cheerio.load(response.data);
-
-    const data = [];
-    $('#qr_ergebnis td.qr_td').each((i, elem) => {
-        data.push($(elem).text());
-    });
-
-    console.log(data);
-
-    const resultData = [];
-
-    for (let i = 0; i < data.length; i = i + 3) {
-        if (data[i] === '16') {
-            resultData.push(data[i + 2]);
-        }
-    }
-
-    console.log(resultData);
-};
-
-execute();
+abfahrt().then(result => console.log(result))
